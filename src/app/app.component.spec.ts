232c2713b3e30a4dfa9a3434adcfb6844f9bc5fc
Derek Wang <birdinforest@gmail.com>
2019-04-27 23:53:16 +1000

    Init.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,47 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {Todo} from './todo';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        // Import FormsModule, otherwise test framework Karma will give a error
+        // complaining about not knowing ngModel.
+        // ref: https://angular.io/docs/ts/latest/guide/testing.html
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  // Hello
+
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'untitled1'`, () => {
+  it(`should have as title 'todo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('untitled1');
-  });
+    expect(app.title).toEqual('todo');
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to untitled1!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
+
+  it('should have a newTodo todo', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]          // Provide a singleton instance of
+                                        // TodoDataService when we ask for it.
+                                        // ref: https://angular.io/guide/dependency-injection
 })
 export class AppComponent {
-  title = 'untitled1';
+  title = 'todo';
+
+  // This property will be two-way bind ty [(ngModel)] in the view, app.component.html.
+  newTodo: Todo = new Todo();
+
+  /**
+   * Ask Angular DI system to inject the dependency associated with the
+   * dependency injection token `TodoDataService` and assign it to property called
+   * `todoDataService`.
+   * Keyword `private` before `todoDataService` is a shorthand to create a private property then
+   * assign it in constructor:
+   * ```ts
+   *      private todoDataService: TodoDataService;
+   *      constructor(private todoDataservice: TodoDataService) {
+   *        this.todoDataService = todoDataService;
+   *      }
+   * ```
+   */
+  constructor(private todoDataservice: TodoDataService) {}
+
+  /**
+   * Service is now available as this.todoDataService
+   */
+  toggleTodoCompelete(todo) {
+    this.todoDataservice.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoDataservice.addTodo(this.newTodo);
+
+    // Reset newTodo property after addition. Since it is two-way bind, the input filed of
+    // in view will be emptied.
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataservice.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo: Todo) {
+    this.todoDataservice.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataservice.getAllTodos();
+  }
 }
